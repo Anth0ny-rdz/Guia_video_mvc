bff2d993ea233969c7b3000bd4d5530294ba0365
Anthony_Rodriguez <anthony.rodriguez.camuendo@udla.edu.ec>
2024-10-09 23:55:19 -0500

    submit 2

--- minicore2-main/minicore2-main/task/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Project, Task
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Project)
-admin.site.register(Task)
